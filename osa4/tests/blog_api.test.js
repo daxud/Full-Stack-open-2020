28bc9b30273eefab8ad01f8e50fb3d1e35ec4684
Akseli Aho <akseli.aho@aalto.fi>
2020-11-22 08:54:05 +0200

    Completed the round 4 completely

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -4,6 +4,7 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const initialBlogs = [
   {
     title: "testi blogi 2",
@@ -21,6 +22,7 @@ const initialBlogs = [
 
 beforeEach(async () => {
   await Blog.deleteMany({})
+  await User.deleteMany({})
   let blogObject = new Blog(initialBlogs[0])
   await blogObject.save()
   blogObject = new Blog(initialBlogs[1])
@@ -53,12 +55,34 @@ test('blogs can be added', async () => {
         url: "www.testi3.fi",
         likes: "300"
     }
-    
+
+    const validUser = {
+      "username": "daxud",
+      "password": "salasana"
+    }
+    //create user
+    const createdUser = await api
+      .post('/api/users')
+      .send(validUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    //login
+    const result = await api
+      .post('/api/login')
+      .send(validUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    //extract the token
+    const token = result.body.token
+    const auth = "bearer " + token
+
     await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-    
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', auth)
+      .expect(201)
+
     const allBlogs = await api.get('/api/blogs')
     //checks if the blog got added to the server
     expect(allBlogs.body).toHaveLength(initialBlogs.length + 1)
@@ -71,10 +95,32 @@ test('blogs can be added without like field', async () => {
         url: "www.testi3.fi",
     }
     
+    const validUser = {
+      "username": "daxud",
+      "password": "salasana"
+    }
+    //create user
+    const createdUser = await api
+      .post('/api/users')
+      .send(validUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    //login
+    const result = await api
+      .post('/api/login')
+      .send(validUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    //extract the token
+    const token = result.body.token
+    const auth = "bearer " + token
+
     await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', auth)
+      .expect(201)
     
     //checks if the value of like is equal to 0
     const allBlogs = await api.get('/api/blogs')
@@ -87,19 +133,55 @@ test('blogs can not be added without title and url fields', async() => {
         author: "akseli3",
         url: "www.testi3.fi",
     }
+    const validUser = {
+      "username": "daxud",
+      "password": "salasana"
+    }
+    //create user
+    const createdUser = await api
+      .post('/api/users')
+      .send(validUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    //login
+    const result = await api
+      .post('/api/login')
+      .send(validUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    //extract the token
+    const token = result.body.token
+    const auth = "bearer " + token
+
     await api
-        .post('/api/blogs')
-        .send(newBlog1)
-        .expect(400)
+      .post('/api/blogs')
+      .send(newBlog1)
+      .set('Authorization', auth)
+      .expect(400)
 
     const newBlog2 = {
         title: "testi blogi 3",
         author: "akseli3"
     }
     await api
-        .post('/api/blogs')
-        .send(newBlog2)
-        .expect(400)
+      .post('/api/blogs')
+      .send(newBlog2)
+      .set('Authorization', auth)
+      .expect(400)
+})
+
+test('blogs can not be added without token', async() => {
+  const newBlog = {
+    title: "testi blogi 3",
+    author: "akseli3",
+    url: "www.testi3.fi",
+  }
+  //sends a post request without a token in the authorization header
+  await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401)
 })
 
 afterAll(() => {
